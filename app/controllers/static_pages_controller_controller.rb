cb4517025d77184692a22de4afc2f6c7fb2170e7
Grey Elerson <grey.l.elerson@gmail.com>
2014-10-18 20:26:44 -0500

    correct StaticPages controller name

--- app/controllers/static_pages_controller_controller.rb
+++
@@ -1,7 +0,0 @@
-class StaticPagesControllerController < ApplicationController
-  def home
-  end
-
-  def help
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   get 'static_pages_controller/home'
 
   get 'static_pages_controller/help'
