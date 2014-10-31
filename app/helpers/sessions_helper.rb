3650048ff1a2bbdb2b223069056580fa19c85ab0
Grey Elerson <grey.l.elerson@gmail.com>
2014-10-30 22:30:46 -0500

    Add useful comments to clarify code

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,7 @@ module SessionsHelper
   # Remembers a user in a persistent session.
   def remember(user)
     user.remember
-    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent.signed[:user_id] = user.id                # permanent() and signed() are just modifiers of the cookies obj
     cookies.permanent[:remember_token] = user.remember_token
   end
 
@@ -16,9 +16,14 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
+    elsif (user_id = cookies.signed[:user_id]) # permanent cookie will last longer than session
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(cookies[:remember_token])  # where line 10 and line 11 come together to make sure 
+                                                                # the "user" variable has the associated :remember_token/digest
+                                                                # BENEFIT: By changing the :remember_token (logging in on another
+                                                                # machine), a "user" can effectievly log-out any other browsers that
+                                                                # are logged in as that "user"... because now the token on that second
+                                                                # party browser will no longer match the digest in the database. 
         login user
         @current_user = user
       end
@@ -30,7 +35,7 @@ module SessionsHelper
     !current_user.nil?
   end
   
-  # Forgets a persistent session
+  # Forgets a persistent session (using :remember_token/digest to identify a client as no longer active)
   def forget(user)
     user.forget
     cookies.delete(:user_id)
