a7ed112900452332ffa7af97bf6f13fe3b2f87cc
Grey Elerson <grey.l.elerson@gmail.com>
2014-11-02 20:48:03 -0600

    Finish user microposts, tests and production configurations

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Google Cloud Storage
+      # Instructions at http://fog.io/storage/
+      :provider              => 'Google',
+      :google_storage_access_key_id     => ENV['GCS_ACCESS_KEY'],
+      :google_storage_secret_access_key => ENV['GCS_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['GCS_BUCKET']
+  end
+end
